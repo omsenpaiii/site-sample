7e554c3a6fa1e1545473c39d1f3e9bfb29a891d0
omtomar2004 <otomar@thesummitgrp.com>
2025-12-18 15:53:13 +0530

    removed separate values

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,6 @@ export default function Home() {
       <SecuritySection />
       <DemoSection />
       <AboutSection />
-      <ValuesSection />
       <CareersSection />
       <ContactSection />
       <Footer />
